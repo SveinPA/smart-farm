135156a005314fe7f3a6818215cac64053176b03
LordManBat <mlamund@stud.ntnu.no>
2025-10-21 16:21:16 +0200

    First draft of Actuator-interface class

--- common/src/main/java/edu/ntnu/bidata/smg/group8/common/actuator/Actuator.java
+++ common/src/main/java/edu/ntnu/bidata/smg/group8/common/actuator/Actuator.java
@@ -2,11 +2,46 @@ package edu.ntnu.bidata.smg.group8.common.actuator;
 
 /**
  * An interface for actuators.
- * Actuator is set as an interface to allow more flexibility in implementations
- * and to avoid code duplication in actuators.
+ * This interface defines what all actuators should be able to do.
  *
  * @author Group 8, MLTA
  * @version 21.10.25
  */
 public interface Actuator {
+
+    /**
+     * Returns the id of the actuator.
+     *
+     * @return id of the actuator
+     */
+    int getId();
+
+    /**
+     * Returns the type of the actuator.
+     *
+     * @return type of the actuator
+     */
+    String getType();
+
+    /**
+     * Checks if the actuator is active.
+     *
+     * @return true if the actuator is active, false otherwise
+     */
+    boolean isActive();
+
+    /**
+     * Activates the actuator.
+     */
+    void activate();
+
+    /**
+     * Deactivates the actuator.
+     */
+    void deactivate();
+
+    /**
+     * Performs the action of the actuator.
+     */
+    void performAction();
 }
