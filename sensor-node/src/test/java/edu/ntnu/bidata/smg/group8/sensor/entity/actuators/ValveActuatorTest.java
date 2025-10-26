7d501943de189700c627ebb34fba084c321b4c10
LordManBat <mlamund@stud.ntnu.no>
2025-10-26 14:30:17 +0100

    minor checkstyle and creation of class changes

---
+++ sensor-node/src/test/java/edu/ntnu/bidata/smg/group8/sensor/entity/actuators/ValveActuatorTest.java
@@ -0,0 +1,11 @@
+package edu.ntnu.bidata.smg.group8.sensor.entity.actuators;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ValveActuatorTest {
+}
