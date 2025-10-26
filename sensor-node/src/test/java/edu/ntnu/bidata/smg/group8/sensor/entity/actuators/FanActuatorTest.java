2fb636ff1fee885cd2564273b1011ff9e76f0195
LordManBat <mlamund@stud.ntnu.no>
2025-10-26 12:29:24 +0100

    created test class for fan actuator

---
+++ sensor-node/src/test/java/edu/ntnu/bidata/smg/group8/sensor/entity/actuators/FanActuatorTest.java
@@ -0,0 +1,9 @@
+package edu.ntnu.bidata.smg.group8.sensor.entity.actuators;
+
+/**
+ * Test class for {@link FanActuator}.
+ *
+ */
+public class FanActuatorTest {
+    //TODO: Implement unit tests for FanActuator
+}
