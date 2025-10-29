72094d3d17284008886678df77c4f9025c6c464f
captnsundy <icsolda@stud.ntnu.no>
2025-10-29 13:41:44 +0100

    Refactor import statements in SensorDataPacketTest for clarity and consistency

--- sensor-node/src/test/java/edu/ntnu/bidata/smg/group8/sensor/infra/SensorDataPacketTest.java
+++ sensor-node/src/test/java/edu/ntnu/bidata/smg/group8/sensor/infra/SensorDataPacketTest.java
@@ -3,7 +3,9 @@ package edu.ntnu.bidata.smg.group8.sensor.infra;
 import edu.ntnu.bidata.smg.group8.sensor.entity.sensors.TemperatureSensor;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * <h3>Unit Tests for SensorDataPacket</h3>
